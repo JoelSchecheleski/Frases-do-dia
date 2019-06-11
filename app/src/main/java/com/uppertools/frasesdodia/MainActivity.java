2e4112f50cbf2571cc5baba5c57f5ced512d01fa
DESKTOP-CU92569\Celere Sistemas <tecnologia@celeresistemas.com.br>
2019-06-11 10:58:15 -0300

    Addinf files

---
+++ app/src/main/java/com/uppertools/frasesdodia/MainActivity.java
@@ -0,0 +1,19 @@
+package com.uppertools.frasesdodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gerarFrase (View view) {
+
+    }
+
+}
